24df713aeda742640c524cc2f8446ae3b1ff658c
arpit-pandey <arpitprince1999@gmail.com>
2021-01-17 18:41:22 +0530

    Adding Styles

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,7 +17,9 @@ $("input[type='text']").keypress(function(event){
 		var todoText= $(this).val();
 		$(this).val("");
 		//create new li and add to ul
-		$("ul").append("<li><span>X</span>"+ todoText+"</li>");
+		$("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></span>"+ todoText+"</li>");
 	}
-
+})
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 })
